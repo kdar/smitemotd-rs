32a1685d88313248244d819fccfac1f5593a984d
kdar <kevin@outroot.com>
2019-03-23 14:14:39 -0400

    Add support for Pushbullet notifications
    The code path is not taken yet.

--- src/notify/pushbullet.rs
+++ src/notify/pushbullet.rs
@@ -1,11 +1,38 @@
-  // let resp: serde_json::Value = reqwest::Client::new()
-  //   .post("https://api.pushbullet.com/v2/pushes")
-  //   .header("Access-Token", env!("PB_ACCESS_TOKEN"))
-  //   .json(&json!({
-  //     "channel_tag": "smitemotd",
-  //     "type": "note",
-  //     "title": motd[0].title.as_ref().unwrap(),
-  //     "body": "*hey*\n_there_\n:) ", //motd[0].description.as_ref().unwrap(),
-  //   }))
-  //   .send()?
-  //   .json()?;
+use std::error::Error;
+
+use reqwest;
+use serde_json::json;
+
+use crate::model::Model;
+
+pub struct Pushbullet {
+  access_token: String,
+  channel_tag: String,
+}
+
+impl Pushbullet {
+  pub fn new(access_token: &str, channel_tag: &str) -> Self {
+    Self {
+      access_token: access_token.to_string(),
+      channel_tag: channel_tag.to_string(),
+    }
+  }
+}
+
+impl super::Notify for Pushbullet {
+  fn notify(&self, m: Model) -> Result<(), Box<Error>> {
+    reqwest::Client::new()
+      .post("https://api.pushbullet.com/v2/pushes")
+      .header("Access-Token", self.access_token.clone())
+      .json(&json!({
+        "channel_tag": &self.channel_tag,
+        "type": "note",
+        "title": m.title,
+        "body": m.to_string(),
+      }))
+      .send()?
+      .json()?;
+
+    Ok(())
+  }
+}
