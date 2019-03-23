c75867e799ac4cf8904d76df784fd446f3df4799
kdar <kevin@outroot.com>
2019-03-23 14:01:52 -0400

    Remove dependency on slack-hook
    Use JSON and reqwest instead.

--- src/notify/slack.rs
+++ src/notify/slack.rs
@@ -1,8 +1,31 @@
 use std::error::Error;
 
-use slack_hook::{Slack as SlackHook, PayloadBuilder, AttachmentBuilder, Field};
+use reqwest;
+use serde_derive::{Deserialize, Serialize};
 
 use crate::model::{self, Model};
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+struct Payload {
+  text: String,
+  username: String,
+  attachments: Vec<Attachment>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+struct Attachment {
+  color: String,
+  fields: Vec<Field>,
+  footer: String,
+  footer_icon: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+struct Field {
+  title: String,
+  value: String,
+  short: bool,
+}
 
 pub struct Slack {
   hook_url: String,
@@ -18,81 +41,79 @@ impl Slack {
 
 impl super::Notify for Slack {
   fn notify(&self, m: Model) -> Result<(), Box<Error>> {
-    let slack = SlackHook::new(self.hook_url.as_str()).unwrap();
+    let mut payload = Payload::default();
 
-    let mut fields = vec![
-      Field {
-        title: "Game mode".to_string(),
-        value: m.game_mode.clone().into(),
-        short: Some(true),
-      }
-    ];
+    let mut fields = vec![Field {
+      title: "Game mode".into(),
+      value: m.game_mode.clone(),
+      short: true,
+    }];
 
-    fields.extend(m.attributes.iter().map(|a| {
-      match a {
-        model::Attr::KeyValue(key, value) => Field {
-          title: key.to_string(),
-          value: value.as_str().into(),
-          short: Some(true),
-        },
-        model::Attr::Key(key) => Field {
-          title: key.to_string(),
-          value: "".into(),
-          short: Some(true),
-        }
-      }
+    fields.extend(m.attributes.iter().map(|a| match a {
+      model::Attr::KeyValue(key, value) => Field {
+        title: key.to_string(),
+        value: value.to_string(),
+        short: true,
+      },
+      model::Attr::Key(key) => Field {
+        title: key.to_string(),
+        value: "".into(),
+        short: true,
+      },
     }));
 
     if !m.team1and2_gods.is_empty() {
       let mut iter = m.team1and2_gods.iter();
-      let v = iter.by_ref().map(|id| m.gods.get(&id).unwrap().name.clone());
+      let v = iter
+        .by_ref()
+        .map(|id| m.gods.get(&id).unwrap().name.clone());
       fields.push(Field {
-        title: "Team 1 and 2".to_string(),
-        value: v.collect::<Vec<_>>().join(", ").into(),
-        short: Some(true),
+        title: "Team 1 and 2".into(),
+        value: v.collect::<Vec<_>>().join(", "),
+        short: true,
       });
     }
 
     if !m.team1_gods.is_empty() {
       let mut iter = m.team1_gods.iter();
-      let v = iter.by_ref().map(|id| m.gods.get(&id).unwrap().name.clone());
+      let v = iter
+        .by_ref()
+        .map(|id| m.gods.get(&id).unwrap().name.clone());
       fields.push(Field {
-        title: "Team 1 only".to_string(),
-        value: v.collect::<Vec<_>>().join(", ").into(),
-        short: Some(true),
+        title: "Team 1 only".into(),
+        value: v.collect::<Vec<_>>().join(", "),
+        short: true,
       });
     }
 
     if !m.team2_gods.is_empty() {
       let mut iter = m.team2_gods.iter();
-      let v = iter.by_ref().map(|id| m.gods.get(&id).unwrap().name.clone());
+      let v = iter
+        .by_ref()
+        .map(|id| m.gods.get(&id).unwrap().name.clone());
       fields.push(Field {
-        title: "Team 2 only".to_string(),
-        value: v.collect::<Vec<_>>().join(", ").into(),
-        short: Some(true),
+        title: "Team 2 only".into(),
+        value: v.collect::<Vec<_>>().join(", "),
+        short: true,
       });
     }
 
-    let attachments = vec![
-      AttachmentBuilder::new("")
-        .color("#3db156")
-        .footer("Smite MOTD")
-        .footer_icon("https://platform.slack-edge.com/img/default_application_icon.png")
-        .fields(fields)
-        .build()
-        .unwrap(),
-    ];
+    payload.text = format!("*{}*\n{}", m.title, m.description);
+    payload.username = "smite".into();
+    payload.attachments = vec![Attachment {
+      color: "#3db156".into(),
+      footer: "Smite MOTD".into(),
+      footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png".into(),
+      fields: fields,
+    }];
 
-    let p = PayloadBuilder::new()
-      .text(format!("*{}*\n{}", m.title, m.description))
-      .username("smite")
-      .attachments(attachments)
-      .build()
-      .unwrap();
+    println!("{}", serde_json::to_string(&payload)?);
 
-    slack.send(&p)?;
+    reqwest::Client::new()
+      .post(&self.hook_url)
+      .json(&payload)
+      .send()?;
 
     Ok(())
   }
 }
-
