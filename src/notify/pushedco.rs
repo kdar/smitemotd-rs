6f6d6b4cb3e1a3fd9cff98f3f580272cee469e22
kdar <kevin@outroot.com>
2019-03-23 14:10:20 -0400

    Add support for pushed.co notifications
    The code path is not called yet.

--- src/notify/pushedco.rs
+++
@@ -1,9 +0,0 @@
-// reqwest::Client::new()
-//   .post("https://api.pushed.co/1/push")
-//   .form(&[
-//     ("app_key", env!("PUSHED_APP_KEY")),
-//     ("app_secret", env!("PUSHED_APP_SECRET")),
-//     ("target_type", "app"),
-//     ("content", &format!("{}: {}", motd[0].title.as_ref().unwrap(), motd[0].description.as_ref().unwrap())),
-//   ])
-//   .send()?;
